60bbaeece71fc394a3cdbbc48be6a7de2c2a3874
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-10-06 17:30:57 +0500

    services page update

---
+++ src/components/Dropdowns.js
@@ -0,0 +1,370 @@
+'use client'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useEffect, useRef, useState } from 'react'
+import { AiOutlinePlusSquare } from 'react-icons/ai'
+
+const letters = [
+    {
+        ltr: "A",
+        objects: [
+            { title: 'Aerosol' },
+            { title: 'Aftershave' },
+            { title: 'Airbag' },
+            { title: 'Alcohol' },
+            { title: 'Ammunition/Ammo' },
+            { title: 'Animals of any form (alive or dead)' },
+            { title: 'Antique/Antiques' },
+            { title: 'Any Hazardous, Combustible, Or Explosive Materials (Any Item That Displays A UN Number)' },
+            { title: 'Any Item Containing Petrol (Liquid, Gas Or Fumes)' },
+            { title: 'Aromatherapy Oil' },
+            { title: 'Ashes' }
+        ]
+    },
+    {
+        ltr: "B",
+        objects: [
+            { title: 'Basin' },
+            { title: 'Battery' },
+            { title: 'Bed / Bed Frame' },
+            { title: 'Biological Samples' },
+        ]
+    },
+    {
+        ltr: "C",
+        objects: [
+            { title: 'Canoe' },
+            { title: 'Carpet' },
+            { title: 'Cash/Credit Cards/Debit Cards' },
+            { title: 'Chainsaw' },
+            { title: 'Cheque Books' },
+            { title: 'Cheques' },
+            { title: 'Christmas Crackers' },
+            { title: 'Cistern' },
+            { title: 'Combust' },
+            { title: 'Counterfeit Currency' },
+            { title: 'Counterfeit Goods' },
+            { title: 'Crackers' },
+            { title: 'Cream' },
+            { title: 'Currency' },
+        ]
+    },
+    {
+        ltr: "D",
+        objects: [
+
+            { title: 'Dangerous Goods' },
+            { title: 'Documents (Outside The EU)' },
+            { title: 'Driving Licence' },
+            { title: 'Drugs (including prescription)' },
+        ]
+    },
+    {
+        ltr: "E",
+        objects: [
+            { title: 'Engines' },
+            { title: 'Explosive' },
+        ]
+    },
+    {
+        ltr: "F",
+        objects: [
+
+            { title: 'Fence' },
+            { title: 'Filth' },
+            { title: 'Fine Art' },
+            { title: 'Fire Extinguisher' },
+            { title: 'Fire Works/Fire Work' },
+            { title: 'Firearm' },
+            { title: 'Firearms' },
+            { title: 'Firearms (Or Parts Thereof) This Includes Replicas, Toys, Antiques, Decommissioned Firearms' },
+            { title: 'Fish (live or dead)' },
+            { title: 'Flat Screen Television' },
+            { title: 'Food Items (Perishable Goods)' },
+            { title: 'Fragile Goods' },
+            { title: 'Fragrance' },
+        ]
+    },
+    {
+        ltr: "G",
+        objects: [
+
+            { title: 'Gas' },
+            { title: 'Gearbox' },
+            { title: 'Gem' },
+            { title: 'Gun' },
+        ]
+    },
+    {
+        ltr: "H",
+        objects: [
+
+            { title: 'Hazardous Goods' },
+            { title: 'Herbal Steroids' },
+            { title: 'High Value Goods' },
+            { title: 'Household Furnishings' },
+            { title: 'Human' },
+            { title: 'Human Organs' },
+            { title: 'Human Remains' },
+        ]
+    },
+    {
+        ltr: "I",
+        objects: [
+            { title: 'Infectious Substances' },
+            { title: 'Insects' },
+            { title: 'Item Described As “Unknown”' },
+        ]
+    },
+    {
+        ltr: "J",
+        objects: [
+            { title: 'Juice' },
+        ]
+    },
+    {
+        ltr: "K",
+        objects: [
+            { title: 'Kayak' },
+            { title: 'Knife' },
+        ]
+    },
+    {
+        ltr: "L",
+        objects: [
+
+            { title: 'Ladder/Step-Ladder' },
+            { title: 'Lighter' },
+            { title: 'Liquid/Cream/Oil' },
+            { title: 'Live Animals/Insects' },
+            { title: 'Lottery Ticket' },
+        ]
+    },
+    {
+        ltr: "M",
+        objects: [
+
+            { title: 'Macbook' },
+            { title: 'Matches (including safety matches and windproof matches)' },
+            { title: 'Magnetised material' },
+            { title: 'Mattress' },
+            { title: 'Mattress (Either Vacuum Packed / Boxed Or Flat)' },
+            { title: 'Meat' },
+            { title: 'Medication' },
+            { title: 'Milk Powder' },
+            { title: 'Mobile Phone with or without Sim to any Residential address in Turkey/Pakistan' },
+            { title: 'Munition' },
+            { title: 'Munitions Of War' },
+        ]
+    },
+    {
+        ltr: "N",
+        objects: [
+
+            { title: 'Nail Varnish' },
+            { title: 'Narcotic Drugs' },
+        ]
+    },
+    {
+        ltr: "O",
+        objects: [
+
+
+            { title: 'Obscene publications and unlawful indecent images' },
+            { title: 'Oxidising materials or organic peroxides (including disinfectants, nitrates and hair dyes or colourants containing peroxide)' },
+        ]
+    },
+    {
+        ltr: "P",
+        objects: [
+
+            { title: 'Paints, wood varnishes and enamels' },
+            { title: 'Paper Currency/Coins' },
+            { title: 'Party Popper' },
+            { title: 'Passport' },
+            { title: 'Perfumes and aftershaves (including eau de parfum and eau de toilette and alcohol-free perfumes, but excluding non-flammable perfumed creams, gels, oils or lotions.)' },
+            { title: 'Perishable items (including flowers, fresh fruit, vegetables and frozen or chilled foodstuffs)' },
+            { title: 'Petrol' },
+            { title: 'Pesticides (e.g. weed killer and any chemical used to kill pests and insects including fly sprays)' },
+            { title: 'Plant/Tree' },
+            { title: 'Poisons, toxic liquids, solids and gases (including substances that are liable to cause death or injury if swallowed or inhaled or by skin contact, including arsenic, cyanide, fluorine, rat poison)' },
+            { title: 'Porn' },
+            { title: 'Precious Metals And Gemstones' },
+            { title: 'Printer Toner (International Parcels Only)' },
+        ]
+    },
+    {
+        ltr: "Q",
+        objects: [
+            { title: '' },
+
+        ]
+    },
+    {
+        ltr: "R",
+        objects: [
+
+            { title: 'Radioactive materials and samples' },
+            { title: 'Remains' },
+            { title: 'Replica Weapon' },
+        ]
+    },
+    {
+        ltr: "S",
+        objects: [
+
+            { title: 'Screen' },
+            { title: 'Seatbelt Tensioner' },
+            { title: 'Seeds/Plant' },
+            { title: 'Segway/Swegway' },
+            { title: 'Settee' },
+            { title: 'Sharp objects (including knives, kitchen utensils and gardening tools)' },
+            { title: 'Shed' },
+            { title: 'Sink' },
+            { title: 'Slush Syrups' },
+            { title: 'Sofa' },
+            { title: 'Stamps (Unless Franked)' },
+            { title: 'Steering Wheel' },
+            { title: 'Steroids' },
+            { title: 'Suitcase' },
+            { title: 'Surf Board' },
+        ]
+    },
+    {
+        ltr: "T",
+        objects: [
+
+            { title: 'Television' },
+            { title: 'Tickets and related advertisements for illegal lotteries' },
+            { title: 'Tobacco/Tobacco Products' },
+            { title: 'Toilet' },
+            { title: 'Toner' },
+            { title: 'Tyre' },
+        ]
+    },
+    {
+        ltr: "U",
+        objects: [
+
+            { title: 'Unknown' },
+            { title: 'Un-Wrapped Metal' },
+        ]
+    },
+    {
+        ltr: "V",
+        objects: [
+
+            { title: 'Vaccines' },
+            { title: 'Vehicle Bonnet' },
+            { title: 'Vehicle Bumper' },
+
+        ]
+    },
+    {
+        ltr: "W",
+        objects: [
+
+            { title: 'Waste, dirt, filth or refuse (including household waste)' },
+            { title: 'Water' },
+            { title: 'Weapon' },
+            { title: 'Wheels' },
+            { title: 'Windscreen' },
+        ]
+    },
+    {
+        ltr: "X",
+        objects: [
+            { title: '' },
+        ]
+    },
+    {
+        ltr: "Y",
+        objects: [
+            { title: '' },
+        ]
+    },
+    {
+        ltr: "Z",
+        objects: [
+            { title: '' },
+        ]
+    },
+
+]
+
+export default function Dropdowns() {
+
+    const [index, setIndex] = useState(0);
+    console.log(index);
+
+    return (
+        <div className="flex flex-wrap justify-center">
+            {letters.map((item, index) => {
+                return (
+                    <Menu as="div" key={index} className="relative inline-block text-left my-5 px-8">
+                        <div className=''>
+                            <Menu.Button className="inline-flex w-full justify-center px-2 py-2 text-sm font-2xl gap-x-1 relative">
+                                <AiOutlinePlusSquare
+                                    className="text-3xl text-black hover:text-[#6C0287]"
+                                    onClick={() => setIndex(index)}
+                                    aria-hidden="true"
+                                />
+                                <span className="text-2xl">{item.ltr}</span>
+                            </Menu.Button>
+                        </div>
+                        <Menu.Items className="z-10 absolute left-0 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-slate-200 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="px-5 py-3 ">
+                                <Menu.Item>
+                                    <div className='flex-1'>
+                                        <ul className="list-disc">
+                                            {letters[index].objects.map((item, index) => {
+                                                return <li key={index}>{item.title}</li>
+                                            })}
+                                        </ul>
+                                    </div>
+                                </Menu.Item>
+
+                            </div>
+
+                        </Menu.Items>
+                    </Menu>
+                )
+            })}
+        </div>
+    )
+}
+
+function EditInactiveIcon(props) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 13V16H7L16 7L13 4L4 13Z"
+                fill="#EDE9FE"
+                stroke="#A78BFA"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
+
+function EditActiveIcon(props) {
+    return (
+        <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M4 13V16H7L16 7L13 4L4 13Z"
+                fill="#8B5CF6"
+                stroke="#C4B5FD"
+                strokeWidth="2"
+            />
+        </svg>
+    )
+}
--- src/components/ServicesMain.js
+++ src/components/ServicesMain.js
@@ -1,8 +1,108 @@
 
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Dropdowns from './Dropdowns'
 
 export default function ServicesMain() {
   return (
-    <div>ServicesMain</div>
+    <div className='bg-[#FFFFFF] w-full'>
+      <div className='bg-[#FFFFFF] max-w-5xl text-black mx-auto flex items-start justify-start px-6 lg:px-4 py-4 lg:py-8'>
+        <div className='justify-start'>
+          <h2 className='font-bold'>SERVICES</h2>
+          <p>We offer a bespoke courier service that meets your business demands</p>
+        </div>
+      </div>
+
+      <div className='bg-[#CCCCCC] text-black mx-auto max-w-8xl flex flex-col items-center px-6 lg:px-4 py-4 lg:py-8 justify-between'>
+        <div className=' flex flex-col gap-x-4 mt-6 max-w-5xl'>
+
+          <div className='lg:flex-1 text-xs '>
+            <p className='mb-4'>ASK Sameday Couriers is based in West Yorkshire that not only serves within the region but also covers the length and breadth of the United Kingdom as well as Europe.</p>
+          </div>
+
+          <div className='lg:flex-1 text-xs'>
+            <p className='mb-4'>We offer a same day service that is highly competitive, delivered on time, fully insured and tracked throughout its delivery. We work with a wide range of businesses in various sectors offering a collection and delivery service by professional and courteous drivers providing a cost-effective solution.</p>
+          </div>
+
+          <Image
+            src="/warehouse.jpg"
+            width={900}
+            height={500}
+            alt="Warehouse"
+            className='mt-5 pb-10'
+          />
+
+        </div>
+
+      </div>
+
+      <div className='bg-[#FFFFFF] text-black mx-auto max-w-8xl flex flex-col items-center px-6 lg:px-4 py-4 lg:py-8 justify-between'>
+        <div className='max-w-5xl'>
+          <h3 className="font-bold mb-5">WHAT WE OFFER</h3>
+          <p className=" mb-5">ASK Sameday Couriers offers a wide range of services:</p>
+          <div className='flex flex-col md:flex-row gap-x-10'>
+            <div className='max-w-2xl flex-1'>
+              <ul className="list-disc">
+                <li>Same Day Nationwide and European Courier Service</li>
+                <li>Priority and Urgent Delivery</li>
+                <li>Monthly or Weekly Contract Runs</li>
+                <li>Timed and Schedules Deliveries</li>
+                <li>Specialise in Small, Short & Long wheel and Luton vans</li>
+                <li>All our vehicles have Full GPS Tracking Facility</li>
+                <li>We have access to a vast network of drivers all across the UK</li>
+                <li>24 Hour / 7 Days a week operational service</li>
+                <li>We offer Competitive Pricing based on genuine quotes</li>
+                <li>Registered with Courier Exchange, APC Overnight and TNT</li>
+                <li>Full AA Breakdown cover</li>
+                <li>Instant Proof of delivery document</li>
+                <li>Full goods in transit insurance</li>
+                <li>No collection fuel surcharge</li>
+                <li>No out of hours or weekend fuel surcharge</li>
+              </ul>
+            </div>
+            <div className='max-w-2xl flex-1'>
+              <ul className="list-disc">
+                <li>Collect within 60* minutes anywhere in the UK (*based on clear traffic with no delays)</li>
+                <li>Immediate account facilities available with flexible payment options</li>
+                <li>A wide range of delivery options, from a single document to a full van load</li>
+                <li>Single point of contact  no call centre</li>
+                <li>Customer service driven</li>
+                <li>Each shipment is delivered with care</li>
+                <li>Professional and courteous drivers</li>
+                <li>Bespoke courier services to meet your needs and demands</li>
+                <li>Overnight Secure Storage with CCTV 25/30 Pallet storage facility</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='bg-[#DDDDDD] text-black mx-auto max-w-8xl flex flex-col items-center px-6 lg:px-4 py-4 lg:py-8 justify-between'>
+        <div className="max-w-5xl">
+
+          <h3 className="font-bold mb-5">PERMISSIBLE ITEMS & BUSINESS SECTORS</h3>
+          <p className=" mb-5">We can handle any type of parcel as long as it is correctly packaged and is not listed in our prohibited* items list shown below. Our courier services covers a wide range of business sectors:</p>
+          <div className='flex flex-col md:flex-row gap-x-10'>
+
+          </div>
+          <p className="text-[#6C0287] mb-5">All items should be packed in appropriate packaging with quilted lining or bubble wrap. Furthermore, if you wish to save the trunk from damage, you should pack it into an extra box.</p>
+        </div>
+      </div>
+
+
+      <div className='bg-[#DBE0D8] text-black mx-auto max-w-8xl flex flex-col items-center px-6 lg:px-4 py-4 lg:py-8 justify-between'>
+        <div className="max-w-5xl">
+
+          <h3 className="font-bold mb-5">*PROHIBITED ITEMS</h3>
+          <p className=" mb-5">Below are examples of items that could be defined as <span className='text-[#6C0287]'>Prohibited</span> dependent on the country of origin, transit and destination which include but are not limited to:</p>
+          <div className='flex flex-col md:flex-row gap-x-10'>
+            <Dropdowns />
+          </div>
+          <p className=" mb-5">For further information on restricted and non-compensation items speak to one of our courier experts by email: info@asksamedaycouriers.co.uk.</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
