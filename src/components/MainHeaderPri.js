28e4fd87da9d61cd84ee6e22f3b8439bc0f80b1d
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-10-17 22:12:43 +0500

    a

--- src/components/MainHeaderPri.js
+++ src/components/MainHeaderPri.js
@@ -14,73 +14,80 @@ export default function MainHeaderPri() {
   // console.log(scrollPosition)
   return (
     <header className="bg-[#6C0287] text-white sticky top-0 z-50">
-      <nav className="mx-auto flex max-w-5xl items-center justify-between pb-2 px-4 lg:px-0 " aria-label="Global">
+      <div>
+        <nav className="mx-auto flex max-w-5xl items-center justify-between pb-2 px-4 lg:px-0 " aria-label="Global">
 
-        <div className="flex lg:flex-1 pl-2">
-          <Link href="/" className="-m-3 p-1 flex flex-row gap-x-3">
-            {/* <Image className="w-full py-3" src="/logo.png" height={100} width={75} alt="logo" /> */}
-            <div className="flex flex-rowitems-start md:items-start gap-x-2">
-              <div className="flex flex-col mt-2">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-auto h-12">
-                  <path d="M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 116 0h3a.75.75 0 00.75-.75V15z" />
-                  <path d="M8.25 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM15.75 6.75a.75.75 0 00-.75.75v11.25c0 .087.015.17.042.248a3 3 0 015.958.464c.853-.175 1.522-.935 1.464-1.883a18.659 18.659 0 00-3.732-10.104 1.837 1.837 0 00-1.47-.725H15.75z" />
-                  <path d="M19.5 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z" />
-                </svg>
+          {/* Logo and Title Lg Screen */}
+          {!mobileMenuOpen && <div className="flex lg:flex-1 pl-2">
+            <Link href="/" className="-m-3 p-1 flex flex-row gap-x-3">
+              <div className="flex flex-rowitems-start md:items-start gap-x-2">
+                <div className="flex flex-col mt-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-auto h-12">
+                    <path d="M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 116 0h3a.75.75 0 00.75-.75V15z" />
+                    <path d="M8.25 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0zM15.75 6.75a.75.75 0 00-.75.75v11.25c0 .087.015.17.042.248a3 3 0 015.958.464c.853-.175 1.522-.935 1.464-1.883a18.659 18.659 0 00-3.732-10.104 1.837 1.837 0 00-1.47-.725H15.75z" />
+                    <path d="M19.5 19.5a1.5 1.5 0 10-3 0 1.5 1.5 0 003 0z" />
+                  </svg>
+                </div>
+                <div className='flex flex-col mt-2 text-md font-bold text-[#ffffff]'>
+                  <span className='text-[#FDAF17]'>Vivo</span>
+                  Courier
+                </div>
               </div>
-              <div className='flex flex-col mt-2 text-md font-bold text-[#ffffff]'>
-                <span className='text-[#FDAF17]'>Vivo</span>
-                Courier
-              </div>
-            </div>
-          </Link>
-        </div>
+            </Link>
+          </div>}
+
+          {/* Mobile menu hamburger menu */}
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md py-[32px] text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-10 w-20 text-white" aria-hidden="true" />
+            </button>
+          </div>
+
+          {/* nav items for lg screen */}
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-8">
+            <Link href="/" className="text-sm font-semibold leading-6 text-white hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300">
+              Home
+            </Link>
+            <Link href="/about" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              About
+            </Link>
+            <Link href="/services" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              Services
+            </Link>
+            <Link href="/fleet" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              Fleet
+            </Link>
+            <Link href="/network" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              Network
+            </Link>
+            <Link href="/quotation" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              Quotation
+            </Link>
+            <Link href="/news" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
+              News
+            </Link>
+            <Link href="/contact" className="text-sm font-semibold leading-6 hover:text-[#FDAF17]  transition-all duration-300 text-white">
+              Contact
+            </Link>
+          </div>
+        </nav>
+      </div>
 
 
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md py-[42px] text-gray-700"
-            onClick={() => setMobileMenuOpen(true)}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-10 w-20 text-white" aria-hidden="true" />
-          </button>
-        </div>
-
-
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-8">
-          <Link href="/" className="text-sm font-semibold leading-6 text-white hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300">
-            Home
-          </Link>
-          <Link href="/about" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            About
-          </Link>
-          <Link href="/services" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            Services
-          </Link>
-          <Link href="/fleet" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            Fleet
-          </Link>
-          <Link href="/network" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            Network
-          </Link>
-          <Link href="/quotation" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            Quotation
-          </Link>
-          <Link href="/news" className="text-sm font-semibold leading-6 hover:text-[#FDAF17] active:text-[#FDAF17] transition-all duration-300 text-white">
-            News
-          </Link>
-          <Link href="/contact" className="text-sm font-semibold leading-6 hover:text-[#FDAF17]  transition-all duration-300 text-white">
-            Contact
-          </Link>
-        </div>
-      </nav>
       <div className='bg-[#FDAF17] h-[10px]'></div>
       {/* Mobile menu */}
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between bg-[#6C0287] px-6 py-6">
+
+
+          {/* Mobile menu - Logo/ title + Close Button */}
+          <div className="flex items-center justify-between bg-[#6C0287] px-6 py-4">
             <Link href="/" className="-m-1.5 p-1.5 flex flex-row gap-x-3">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-auto h-16 ">
                 <path d="M3.375 4.5C2.339 4.5 1.5 5.34 1.5 6.375V13.5h12V6.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM13.5 15h-12v2.625c0 1.035.84 1.875 1.875 1.875h.375a3 3 0 116 0h3a.75.75 0 00.75-.75V15z" />
@@ -100,6 +107,9 @@ export default function MainHeaderPri() {
             </button>
           </div>
           <div className='bg-[#FDAF17] h-[10px]'></div>
+
+
+          {/* Mobile menu - Menu Items */}
           <div className="flow-root px-6 py-6">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
