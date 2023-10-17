796ac767f2f60f1af369fd31e23b7f823c1d258f
Shamail Bin Wasif <Shamail130silverhawk@gmail.com>
2023-10-17 18:23:05 +0500

    Vivo Corporation Ltd 2

--- src/components/MiniHeaderPri.js
+++ src/components/MiniHeaderPri.js
@@ -12,21 +12,21 @@ import { FaHouseSignal } from 'react-icons/fa6'
 export default function MiniHeaderPri() {
   return (
     <div className='hidden lg:block w-full bg-[#6C0287]'>
-      <div className="mx-auto max-w-5xl flex items-center pt-2 justify-between text-white text-sm font-bold align-middle">
+      <div className="mx-auto max-w-5xl flex items-center pt-2 justify-between text-white font-bold align-middle">
         <div className='flex gap-x-5'>
           <div className="flex flex-row gap-x-2 ">
-            <GiRotaryPhone className='text-[#FDAF17] text-xl' />
-            <p className="hover:text-[#FDAF17] transition-all duration-300">03xx xxxxxxx</p>
+            <GiRotaryPhone className='text-[#FDAF17] text-lg' />
+            <p className="hover:text-[#FDAF17] transition-all duration-300 text-xs font-thin-">03xx xxxxxxx</p>
           </div>
           <div className="flex flex-row gap-x-2">
             <SlEnvolopeLetter className='text-[#FDAF17] text-lg' />
-            <p className="hover:text-[#FDAF17] transition-all duration-300">info@VivoCouriers.co.uk</p>
+            <p className="hover:text-[#FDAF17] transition-all duration-300 text-xs font-thin-">info@VivoCouriers.co.uk</p>
           </div>
         </div>
         <div className='flex gap-x-5 lg:justify-end'>
           <div className="flex flex-row gap-x-2">
-            <FaHouseSignal className='text-[#FDAF17] text-xl' />
-            <p className="hover:text-[#FDAF17] transition-all duration-300">Subscribe</p>
+            <FaHouseSignal className='text-[#FDAF17] text-lg' />
+            <p className="hover:text-[#FDAF17] transition-all duration-300 text-xs font-thin-">Subscribe</p>
           </div>
           <div className="flex flex-row gap-x-2 items-center">
             <Link target="_blank" href={'https://www.fb.com//'} className="text-xl hover:text-[#FDAF17] transition-all duration-300"><BiLogoFacebook /></Link>
